05a8d407a7bd40f0b3b05e740abf615776025bfd
Ahmed Abdel hameed <gt715yt@gmail.com>
2024-01-27 03:05:55 +0200

    Update Main.java

--- Prefix-Evaluation/src/main/java/org/assignment/Main.java
+++ Prefix-Evaluation/src/main/java/org/assignment/Main.java
@@ -3,8 +3,7 @@ package org.assignment;
 public class Main {
     public static void main(String[] args) {
 
-        String prefix = Prefix.getPrefix("file.xml");
-
+        String prefix = Prefix.getPrefix("file.xml");//dont forget to change the path from the resources to get differnt output
         if (prefix != null) {
             System.out.println("The prefix is:" + prefix);
             String infix = Evaluation.getInfix(prefix);
